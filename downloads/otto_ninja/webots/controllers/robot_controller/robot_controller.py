0219e13f7c892ce0d94aff531dded94c416d33b4
mdecycu <mdecycuorg@gmail.com>
2025-07-17 17:12:09 +0800

    add otto_ninja webots simulation

---
+++ downloads/otto_ninja/webots/controllers/robot_controller/robot_controller.py
@@ -0,0 +1,34 @@
+"""robot_controller controller."""
+
+# You may need to import some classes of the controller module. Ex:
+#  from controller import Robot, Motor, DistanceSensor
+from controller import Robot
+
+# create the Robot instance.
+robot = Robot()
+
+# get the time step of the current world.
+timestep = int(robot.getBasicTimeStep())
+
+# You should insert a getDevice-like function in order to get the
+# instance of a device of the robot. Something like:
+right_motory = robot.getDevice('right_motory')
+left_motory = robot.getDevice('left_motory')
+#  ds = robot.getDevice('dsname')
+#  ds.enable(timestep)
+
+# Main loop:
+# - perform simulation steps until Webots is stopping the controller
+while robot.step(timestep) != -1:
+    # Read the sensors:
+    # Enter here functions to read sensor data, like:
+    #  val = ds.getValue()
+
+    # Process sensor data here.
+
+    # Enter here functions to send actuator commands, like:
+    right_motory.setPosition(-1.7)
+    left_motory.setPosition(1.7)
+    pass
+
+# Enter here exit cleanup code.
